4e259f8ef3a1837750db73ef2983bcdba84bf0ce
AntoRend <antonio@toroto.mx>
2022-11-17 13:26:26 -0600

    Endpoints and fixes

---
+++ pages/api/farmers.js
@@ -0,0 +1,14 @@
+import { farmers } from '../../lib/useCases'
+import dbConnect from '../../lib/mongo/dbConnect'
+
+export default async function farmersData (req, res) {
+  try {
+    if (req.method !== 'GET') throw new Error('Method not supported')
+    await dbConnect()
+    const data = await farmers.getAll()
+    res.status(200).json({ data })
+  } catch (error) {
+    console.log(error.message)
+    res.status(400).json({ errorx:  error.message })
+  }
+}
